e1838b6d953de176b38b3fe8c9df9214b5659527
ksenmel <ksemelashenko@gmail.com>
2025-09-11 22:39:29 +0300

    add one spark session for tests

--- mst_pyspark/test/test_boruvka.py
+++ mst_pyspark/test/test_boruvka.py
@@ -1,53 +1,49 @@
-from pyspark.sql import SparkSession
+import pytest
 
 from boruvka.boruvka import boruvka_mst
+from pyspark.sql import SparkSession
 
-def test_graph_1():
-    spark = SparkSession.builder.appName("BoruvkaMST").getOrCreate()
-    
+
+@pytest.fixture(scope="session")
+def spark():
+    return SparkSession.builder.appName("BoruvkaMST").getOrCreate()
+
+def test_graph_1(spark):
     edges = [
-            (0, 1, 4.0),
-            (0, 2, 2.0),
-            (1, 2, 1.0),
-            (1, 3, 5.0),
-            (2, 3, 8.0),
-            (2, 4, 10.0),
-            (3, 4, 2.0),
-            (3, 5, 6.0),
-            (4, 5, 3.0)
+        (0, 1, 4.0),
+        (0, 2, 2.0),
+        (1, 2, 1.0),
+        (1, 3, 5.0),
+        (2, 3, 8.0),
+        (2, 4, 10.0),
+        (3, 4, 2.0),
+        (3, 5, 6.0),
+        (4, 5, 3.0)
     ]
 
     edges_df = spark.createDataFrame(edges, ["u", "v", "weight"])
     num_vertices = 6
 
-    #TODO compare edges
-        
     mst_edges, total_weight = boruvka_mst(spark, edges_df, num_vertices)
 
     assert total_weight == 13
 
-
-def test_graph_2():
-    spark = SparkSession.builder.appName("BoruvkaMST").getOrCreate()
-    
+def test_graph_2(spark):
     edges = [
-            (0, 1, 10.0),
-            (0, 2, 6.0),
-            (0, 3, 5.0),
-            (1, 3, 15.0),
-            (2, 3, 4.0),
-            (1, 4, 3.0),
-            (3, 4, 8.0),
-            (4, 5, 7.0),
-            (3, 5, 12.0)
-        ]
+        (0, 1, 10.0),
+        (0, 2, 6.0),
+        (0, 3, 5.0),
+        (1, 3, 15.0),
+        (2, 3, 4.0),
+        (1, 4, 3.0),
+        (3, 4, 8.0),
+        (4, 5, 7.0),
+        (3, 5, 12.0)
+    ]
     
     edges_df = spark.createDataFrame(edges, ["u", "v", "weight"])
     num_vertices = 6
         
     mst_edges, total_weight = boruvka_mst(spark, edges_df, num_vertices)
 
-    #TODO compare edges
-
     assert total_weight == 27
-
