aa73d7b73b04e2931e555df78ed397caeda82d60
ksenmel <ksemelashenko@gmail.com>
2025-10-13 18:04:31 +0300

    refactor test

--- triangles/spla_tc/tests/test.py
+++ triangles/spla_tc/tests/test.py
@@ -1,18 +1,21 @@
 from pyspla import *
 
 from src.sandia import sandia
+from src.burkhardt import burkhardt
 
-rows = [1, 2, 2, 3, 4, 5, 5]
-cols = [0, 0, 1, 1, 3, 3, 4]
-vals = [1, 1, 1, 0, 1, 1, 1]
+tri_rows = [1, 2, 2, 3, 4, 5, 5]
+tri_cols = [0, 0, 1, 1, 3, 3, 4]
+tri_vals = [1, 1, 1, 0, 1, 1, 1]
 
-# rows = [0, 0, 1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 5]
-# cols = [1, 2, 0, 2, 3, 0, 1, 1, 4, 5, 3, 5, 3, 4]
-# vals = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+rows = [0, 0, 1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 5]
+cols = [1, 2, 0, 2, 3, 0, 1, 1, 4, 5, 3, 5, 3, 4]
+vals = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
 M = Matrix.from_lists(rows, cols, vals, shape=(6, 6), dtype=INT)
-print(M)
+N = Matrix.from_lists(tri_rows, tri_cols, tri_vals, shape=(6, 6), dtype=INT)
 
-count = sandia(M)
+count = burkhardt(M)
+tri_count = sandia(N)
 
-print(count)
+print(count) # 2
+print(tri_count) # 2
