552f922b13bc68703085c5bd1fc98338c335e9c0
raf-nr <nurmuhametovrafik04@gmail.com>
2025-11-04 01:03:08 +0300

    implement boruvka on graphblas

---
+++ cpp/graphblas/mst/algo.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "algorunner.h"
+#include "graph.h"
+
+#include <GraphBLAS.h>
+#include <LAGraph.h>
+#include <vector>
+#include <chrono>
+
+struct GBTree {
+    GrB_Index num_vertices;
+    std::vector<int> parent;
+    uint64_t total_weight;
+};
+
+class GBMST : public AlgoRunner<GBTree> {
+public:
+    GBMST();
+
+    void RunAlgo(const GBGraph& graph) override;
+    const GBTree& GetResult() const override;
+    std::chrono::milliseconds GetExecTime() const;
+
+private:
+    void ComputeMST(const GBGraph& graph);
+    void ParseResult();
+
+    char msg_[LAGRAPH_MSG_LEN];
+    mutable GrB_Matrix mst_matrix_{nullptr};
+    mutable GrB_Vector component_vec_{nullptr};
+    mutable GBTree result_;
+    std::chrono::milliseconds exec_time_{0};
+    mutable bool parsed_{false};
+};
--- cpp/graphblas/mst/main.cpp
+++ cpp/graphblas/mst/main.cpp
@@ -1,6 +1,20 @@
+#include "algo.h"
 #include <iostream>
+#include <LAGraph.h>
+#include <LAGraphX.h>
+
+#include "parser.h"
 
 int main() {
-    std::cout << "Hello world" << std::endl;
+    GrB_init(GrB_NONBLOCKING);
+
+    Parser parser;
+    GBGraph graph = parser.ParseDIMACS("/Users/rafik/Desktop/USA-road-d.NY.gr");
+    std::cout << "Graph: " << graph.n_nodes << ' ' << graph.n_arcs << std::endl;
+
+    GBMST mst;
+    mst.RunAlgo(graph);
+    std::cout << "MST total weight: " << mst.GetResult().total_weight << std::endl;
+
     return 0;
 }
