eb25ed1b3918397479c5c4097a0fd5422c055194
Rafik Nurmuhametov <nrrafik@10.216.24.128-spb.dhcp.yndx.net>
2025-11-07 20:35:14 +0300

    implement spla runners

--- cpp/spla/msbfs/main.cpp
+++ cpp/spla/msbfs/main.cpp
@@ -5,9 +5,14 @@
 #include "msbfs_config.h"
 #include "parser.h"
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <path_to_configs_dir>\n";
+        return 1;
+    }
+
     MSBFSConfig config =
-            MSBFSConfig::Parse("/home/maybenotilya/proj/graphs_analysis/configs/msbfs.ini");
+        MSBFSConfig::Parse(std::string(argv[1]) + "/msbfs.ini");
     Parser parser;
     SPLAGraph graph = parser.ParseDIMACS(config.GetGraphPath(), false);
     MSBFSRunner runner(config.GetStartingVertices());
